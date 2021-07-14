481bb06be5c0ae7279e333c1ebc9eb77b31759e8
Diansley Fernandes de Lima <diansley2@hotmail.com>
2021-07-14 15:32:26 -0300

    término do formulário de criação de conta

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
                 </div> <!--form-element-->
 
                 <div class="form-element">
-                    <input type="submit" name="acao" value="Enviar">
+                    <input type="submit" name="acao" value="Entrar">
                 </div> <!--form-element-->
 
             </form> <!--form-login-->
@@ -81,6 +81,56 @@
                     <div class="w100">
                         <input placeholder="Nova senha" type="password">
                     </div> <!--w100-->
+
+                    <div class="w100">
+
+                        <h2>Data de nascimento</h2>
+
+                        <select name="nascimento-dia" class="nascimento">
+                            <?php
+                                for($i=1; $i <= 31; $i++) {
+                            ?>
+                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                            <?php } ?>
+                        </select>
+
+                        <select name="nascimento-mes" class="nascimento">
+                            <option value="1">Janeiro</option>
+                            <option value="2">Fevereiro</option>
+                            <option value="3">Março</option>
+                            <option value="4">Abril</option>
+                            <option value="5">Maio</option>
+                            <option value="6">Junho</option>
+                            <option value="7">Julho</option>
+                            <option value="8">Agosto</option>
+                            <option value="9">Setembro</option>
+                            <option value="10">Outubro</option>
+                            <option value="11">Novembro</option>
+                            <option value="12">Dezembro</option>
+                        </select>
+
+                        <select name="nascimento-ano" class="nascimento">
+                        <?php
+                                for($i=1900; $i <= 2021; $i++) {
+                            ?>
+                            <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                            <?php } ?>
+                        </select>
+
+                        <div class="clear"></div>
+                    </div> <!--w100-->
+
+                    <div class="w100">
+                        <div class="input-radio">
+                            <input type="radio" name="sexo" value="masculino">
+                            <h2>Masculino</h2>
+                        </div> <!--input-radio-->
+                        <div class="input-radio">
+                            <input type="radio" name="sexo" value="feminino">
+                            <h2>Feminino</h2>
+                        </div> <!--input-radio-->
+                        <div class="clear"></div>
+                    </div>
 
                     <div class="w100">
                         <input type="submit" name="acao" value="Cadastre-se">
